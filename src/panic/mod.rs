2a143a264f2655927db5d7c00f6be768a87e715f
r1TOASTER <uritsamir@gmail.com>
2025-06-24 12:21:20 +0300

    fixed panic handler for tests and general

--- src/panic/mod.rs
+++ src/panic/mod.rs
@@ -1,7 +1,7 @@
 use core::intrinsics::abort;
 use core::panic::PanicInfo;
 
-#[cfg_attr(not(test), panic_handler)]
+#[panic_handler]
 pub fn panic(_info: &PanicInfo) -> ! {
     // TODO: panic message print to console
     abort()
