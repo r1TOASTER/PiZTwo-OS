01943050805c19e526d2431050231327ea9b68d3
r1TOASTER <uritsamir@gmail.com>
2025-06-24 18:42:13 +0300

    started gpio funtions with mmio integration

--- src/memory/mmio/mod.rs
+++ src/memory/mmio/mod.rs
@@ -2,15 +2,16 @@ use core::marker::Sized;
 
 // TODO: maybe about 8/16 bit scenerio where use ptr offsets / no offsets with bit-wise to keep other register bits info
 
-trait RegSized: Sized {
-    unsafe fn read(addr: usize) -> Self;
-    unsafe fn write(addr: usize, value: Self);
+pub trait RegSized: Sized {
+    unsafe fn mmio_read(addr: usize) -> Self;
+    unsafe fn mmio_write(addr: usize, value: Self);
 }
+
 macro_rules! rdwr {
-    ($t:ident($(reg: $reg_cls:ident, read: $patrd:literal, write: $patwr:literal),* $(,)?)) => {
+    ($t:ident($(reg: $reg_cls:ident, mmio_read: $patrd:literal, mmio_write: $patwr:literal),* $(,)?)) => {
         #[cfg(target_arch = "aarch64")]
         impl RegSized for $t {
-            unsafe fn read(addr: usize) -> $t {
+            unsafe fn mmio_read(addr: usize) -> $t {
                 unsafe {
                     $(#[cfg(target_arch = "aarch64")] {
                         let val: $t;
@@ -19,7 +20,7 @@ macro_rules! rdwr {
                     })*
                 }
             }
-            unsafe fn write(addr: usize, val: $t) {
+            unsafe fn mmio_write(addr: usize, val: $t) {
                 unsafe {
                     $(#[cfg(target_arch = "aarch64")] { core::arch::asm!($patwr, addr = in(reg) addr, val = in($reg_cls) val); })*
                 }
@@ -29,14 +30,14 @@ macro_rules! rdwr {
 }
 
 rdwr!(u8(
-    reg: reg, read: "ldrb {val}, [{addr}]", write: "strb {val}, [{addr}]",
+    reg: reg, mmio_read: "ldrb {val}, [{addr}]", mmio_write: "strb {val}, [{addr}]",
 ));
 rdwr!(u16(
-    reg: reg, read: "ldrh {val}, [{addr}]", write: "strh {val}, [{addr}]",
+    reg: reg, mmio_read: "ldrh {val}, [{addr}]", mmio_write: "strh {val}, [{addr}]",
 ));
 rdwr!(u32(
-    reg: reg, read: "ldrw {val}, [{addr}]", write: "strw {val}, [{addr}]",
+    reg: reg, mmio_read: "ldrw {val}, [{addr}]", mmio_write: "strw {val}, [{addr}]",
 ));
 rdwr!(u64(
-    reg: reg, read: "ldr {val}, [{addr}]", write: "str {val}, [{addr}]",
+    reg: reg, mmio_read: "ldr {val}, [{addr}]", mmio_write: "str {val}, [{addr}]",
 ));
--- src/peripherals/gpio/mod.rs
+++ src/peripherals/gpio/mod.rs
@@ -1,5 +1,3 @@
-// TODO: change pin direction
-// TODO: change pin alternate function
 // TODO: change pin pull state
 // TODO: pull up / down - clock ?
 // TODO: pin level?
@@ -7,3 +5,94 @@
 // TODO: rising / falling edge detect - enable / disable
 // TODO: detect high / low - enable / disable
 // TODO: pin async - rising / falling edge - enable / disable
+
+use crate::memory::mmio::RegSized;
+use core::{{marker::Copy}, {clone::Clone}, {fmt::Debug}};
+
+const GPFSEL0: usize = 0x7E20_0000;
+
+#[derive(Copy, Clone, Debug)]
+pub enum GpioPin {
+    Pin00 = 0,
+    Pin01,
+    Pin02,
+    Pin03,
+    Pin04,
+    Pin05,
+    Pin06,
+    Pin07,
+    Pin08,
+    Pin09,
+    Pin10,
+    Pin11,
+    Pin12,
+    Pin13,
+    Pin14,
+    Pin15,
+    Pin16,
+    Pin17,
+    Pin18,
+    Pin19,
+    Pin20,
+    Pin21,
+    Pin22,
+    Pin23,
+    Pin24,
+    Pin25,
+    Pin26,
+    Pin27,
+    Pin28,
+    Pin29,
+    Pin30,
+    Pin31,
+    Pin32,
+    Pin33,
+    Pin34,
+    Pin35,
+    Pin36,
+    Pin37,
+    Pin38,
+    Pin39,
+    Pin40,
+    Pin41,
+    Pin42,
+    Pin43,
+    Pin44,
+    Pin45,
+    Pin46,
+    Pin47,
+    Pin48,
+    Pin49,
+    Pin50,
+    Pin51,
+    Pin52,
+    Pin53
+}
+
+// defenitions by GPFSELn, page 91
+#[derive(Copy, Clone, Debug)]
+pub enum GpioState {
+    Input = 0b000,
+    Output = 0b001,
+    Alt0 = 0b100,
+    Alt1 = 0b101,
+    Alt2 = 0b110,
+    Alt3 = 0b111,
+    Alt4 = 0b011,
+    Alt5 = 0b010
+}
+
+// TODO: write unit tests
+pub fn gpio_select_mode(pin: GpioPin, state: GpioState) {
+    let reg_offset = ((pin as u8) / 10) * 4; // 32 bit register, offset times 4 + base of GPFSEL0
+    let reg = GPFSEL0 + reg_offset as usize;
+
+    let bit_offset = ((pin as u8) % 10) * 3; // every mode is 3 bit, offset times 3 + base of register (bit 0)
+
+    let mut og_val = unsafe { u32::mmio_read(reg) };
+    
+    og_val &= 0b111 << bit_offset;
+    og_val |= ((state as u8) << bit_offset) as u32;
+
+    unsafe { u32::mmio_write(reg, og_val); }
+}
