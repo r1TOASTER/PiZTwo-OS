03ea2114f9d7eb3ca6972fc56a97b8a3aecb125e
r1TOASTER <uritsamir@gmail.com>
2025-06-26 22:17:18 +0300

    testing SWI on semihosting qemu

--- src/kernel.rs
+++ src/kernel.rs
@@ -17,18 +17,6 @@ pub fn kernel_main() -> ! {
 
     // some semihosting API testing from the main kernel entry point (after entry.S initialliztions)
 
-    let char = 'A' as u8;
-    let char_addr = &char as *const u8;
-    
-    // try to write the char A to the console using SWI on ARM
-    unsafe {
-        core::arch::asm!("
-            mov x0, #0x03
-            mov x1, {}
-            hlt #0xF000
-        ", in(reg) char_addr)
-    };
-
     // exit QEMU using SWI on ARM
     unsafe { 
         core::arch::asm!("
