ab40c9f41c417e889aa5128558e13901e376d296
r1TOASTER <uritsamir@gmail.com>
2025-07-04 10:25:02 +0300

    add schedualr todos, and syscall/thread files

---
+++ kernel/src/cpu/schedualer.rs
@@ -0,0 +1,4 @@
+// TODO: Support for AArch32 thread
+// TODO: Thread stack size / Thread pool size
+// TODO: Thread structure that holds info
+// TODO: Maybe create a thread.rs file to hold that info with consts, and add process creating syscall?
