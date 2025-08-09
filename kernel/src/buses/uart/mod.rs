d2866950da1693006f15232d1dc9177d15cbaf5f
r1TOASTER <uritsamir@gmail.com>
2025-08-09 13:31:56 +0300

    some uart config - not done

--- kernel/src/buses/uart/mod.rs
+++ kernel/src/buses/uart/mod.rs
@@ -6,6 +6,34 @@ use core::option::Option;
 // TODO: maybe global struct for peripherals / buses info
 // TODO uart1 functions - init gpio, init uart, enable / disable, recv / send, console wrapper?
 
+// enum to define which peripheral to access (0 / 1)
+#[repr(u8)]
+pub(crate) enum UartLine {
+    UART0 = 0,
+    UART1
+}
+
+pub(crate) enum BaudRate {
+    BAUD_4800,
+    BAUD_9600,
+    BAUD_19200,
+    BAUD_38400,
+    BAUD_57600,
+    BAUD_115200,
+    BAUD_CUSTOM(u32),
+}
+
+pub(crate) enum StopBit {
+    STOP_ONE,
+    STOP_TWO,
+}
+
+pub(crate) struct UartBus {
+    bus_num: UartLine,
+    baud_rate: Option<BaudRate>,
+    stop_bit: Option<StopBit>,
+}
+
 // internal function to setup gpios 14 and 15
 fn uart1_gpio_init() {
     // set pins direction
